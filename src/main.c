748fc5fa07a0415689f2dfacc4e12d4570545c43
Sandip Karekar <sandip.karekar@gmail.com>
2025-09-21 13:09:50 +0530

    Made addition function for double numbers

--- src/main.c
+++ src/main.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
+float add(float a, float b);
+
 int main(void)
 {
-    int x = 10;
-    int y = 20;
-    printf("The addition of numbers %d and %d is %d", x, y, add(x,y));
+    double x = 10.0f;
+    double y = 20.0f;
+    printf("The addition of numbers %f and %f is %f", x, y, add(x,y));
     return 0;
 }
 
-int add(int a, int b)
+float add(float a, float b)
 {
     return a + b;
 }
