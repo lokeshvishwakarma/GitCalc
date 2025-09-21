73360ef61595357f9f518eae3eee13c3f84835b5
radhikavishwakarma <radhikavish103@gmail.com>
2025-09-21 13:08:33 +0530

    added subtract function

--- src/main.c
+++ src/main.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 
+int add(int a, int b);
+float subtract(float a, float b);
+
 int main(void)
 {
-    int x = 10;
-    int y = 20;
-    printf("The addition of numbers %d and %d is %d", x, y, add(x,y));
+    double x = 10;
+    double y = 20;
+    printf("The addition of numbers %f and %f is %f\n", x, y, add(x,y));
+    printf("The subtraction of numbers %f and %f is %f\n", x, y, subtract(x,y));
     return 0;
 }
 
 int add(int a, int b)
 {
     return a + b;
+}
+
+float subtract(float a, float b)
+{
+    return a - b;
 }
