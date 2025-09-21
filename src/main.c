89821c799062dcd4b33e9a500af4521a06bf45a2
Sandip Karekar <sandip.karekar@gmail.com>
2025-09-21 12:53:00 +0530

    Added addition function for two integers

--- src/main.c
+++ src/main.c
@@ -2,6 +2,13 @@
 
 int main(void)
 {
-    
+    int x = 10;
+    int y = 20;
+    printf("The addition of numbers %d and %d is %d", x, y, add(x,y));
     return 0;
+}
+
+int add(int a, int b)
+{
+    return a + b;
 }
